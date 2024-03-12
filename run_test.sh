d1a3709bfd5dec53b287fe85d6112710afde61c4
Lamin Sanneh <lamin.evra@gmail.com>
2024-03-12 16:12:24 +0000

    Add test runner bash file

---
+++ run_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+php artisan test --filter PhotosControllerTest
+# php artisan test --filter testUploadPhotoFileMissing
+# php artisan test --filter testUploadPhoto
+# php artisan test --filter testUploadPhoto
+# php artisan test --filter TagsControllerTest
+# php artisan test --group=group1
+# php artisan test
